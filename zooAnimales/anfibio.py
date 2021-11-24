6fba7c716cf5b9a4fdbb78bc5420405936e232e9
jumartineze <jumartineze@unal.edu.co>
2021-11-23 23:24:43 -0500

    Commit

---
+++ zooAnimales/anfibio.py
@@ -0,0 +1,45 @@
+from animal import Animal
+
+class Anfibio(Animal):
+    _listado = []
+    ranas = 0
+    salamandras = 0
+
+    def __init__(self, nombre, edad, habitat, genero, colorPiel, venenoso):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPiel = colorPiel
+        self._venenoso = venenoso
+        Anfibio._listado.append(self)
+
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+
+    def getColorPiel(self):
+        return self._colorPiel
+    def setColorPiel(self, colorPiel):
+        self._colorPiel = colorPiel
+
+    def isVenenoso(self):
+        return self._venenoso
+    def setVenenoso(self, venenoso):
+        self._venenoso = venenoso
+
+    def movimiento(self):
+        return "saltar"
+
+    @classmethod
+    def cantidadAnfibios(cls):
+        return len(cls._listado)
+
+    @classmethod
+    def crearRana(cls, nombre, edad, genero):
+        rana = Anfibio(nombre, edad, "selva", genero, "rojo", True)
+        cls.ranas += 1
+        return rana
+
+    @classmethod
+    def crearSalamandra(cls, nombre, edad, genero):
+        salamandra = Anfibio(nombre, edad, "selva", genero, "negro y amarillo", False)
+        cls.salamandras += 1
+        return salamandra 
